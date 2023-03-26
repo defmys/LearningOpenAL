b3d657ea0abca1a0f946e6f0c95bce5bff58b596
tingchenma <tingchenma@tencent.com>
2023-03-26 23:34:45 +0800

    Added libsndfile and a test wave file.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,25 @@
 
 #include "AL/alc.h"
 #include "AL/al.h"
+#include "sndfile.h"
 
 int main(int argc, const char** argv) 
 {
-    std::cout << "Hello" << std::endl;
-
     ALCdevice* device = alcOpenDevice(nullptr);
     assert(device);
+
+    std::string fullPath(argv[0]);
+    std::string exeDir = fullPath.substr(0, fullPath.rfind("/") + 1);
+
+    std::string wavFile = exeDir + "resource/iamtheprotectorofthissystem.wav";
+    SF_INFO sndInfo{};
+    std::cout << wavFile << std::endl;
+    SNDFILE* sndFile = sf_open(wavFile.c_str(), SFM_READ, &sndInfo);
+    assert(sndFile);
+    if (sndFile)
+    {
+        sf_close(sndFile);
+    }
 
     return 0;
 }
