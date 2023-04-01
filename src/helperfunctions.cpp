7f3a9e02f638e46521a6c0a56c1c489852365c85
tingchenma <tingchenma@tencent.com>
2023-04-01 23:45:44 +0800

    Added al helper functions.

--- src/helperfunctions.cpp
+++ src/helperfunctions.cpp
@@ -5,31 +5,64 @@
 
 #include <iostream>
 
-bool check_alc_errors(const char* filename, uint32_t line, ALCdevice* device)
+bool check_al_errors(const char* filename, uint32_t line)
 {
-    ALenum error = alcGetError(device);
+    ALenum error = alGetError();
     if (error != AL_NO_ERROR)
     {
         std::cerr << "AL Error: (" << filename << ":" << line << ")" << std::endl;
         switch (error)
         {
-        case AL_INVALID_NAME: 
-            std::cerr << "AL_INVALID_NAME";
-            break;
         case AL_INVALID_ENUM:
             std::cerr << "AL_INVALID_ENUM";
             break;
-        case AL_INVALID_VALUE:
-            std::cerr << "AL_INVALID_VALUE";
+        case AL_INVALID_NAME:
+            std::cerr << "AL_INVALID_NAME";
             break;
         case AL_INVALID_OPERATION:
             std::cerr << "AL_INVALID_OPERATION";
+            break;
+        case AL_INVALID_VALUE:
+            std::cerr << "AL_INVALID_VALUE";
             break;
         case AL_OUT_OF_MEMORY:
             std::cerr << "AL_OUT_OF_MEMORY";
             break;
         default:
-            std::cerr << "Unknown AL Error.";
+            break;
+        }
+        std::cerr << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool check_alc_errors(const char* filename, uint32_t line, ALCdevice* device)
+{
+    ALCenum error = alcGetError(device);
+    if (error != ALC_NO_ERROR)
+    {
+        std::cerr << "ALC Error: (" << filename << ":" << line << ")" << std::endl;
+        switch (error)
+        {
+        case ALC_INVALID_CONTEXT: 
+            std::cerr << "ALC_INVALID_CONTEXT";
+            break;
+        case ALC_INVALID_DEVICE:
+            std::cerr << "ALC_INVALID_DEVICE";
+            break;
+        case ALC_INVALID_ENUM:
+            std::cerr << "ALC_INVALID_ENUM";
+            break;
+        case ALC_INVALID_VALUE:
+            std::cerr << "ALC_INVALID_VALUE";
+            break;
+        case ALC_OUT_OF_MEMORY:
+            std::cerr << "ALC_OUT_OF_MEMORY";
+            break;
+        default:
+            std::cerr << "Unknown ALC Error.";
         }
         std::cerr << std::endl;
         return false;
--- src/helperfunctions.h
+++ src/helperfunctions.h
@@ -5,22 +5,41 @@
 
 struct ALCdevice;
 
-bool check_alc_errors(const char* filename, uint32_t line, ALCdevice* device);
+bool check_al_errors(const char* filename, uint32_t line);
 
 template<typename alFunction, typename ReturnType, typename... Params>
-auto alcCallImpl(const char* filename, uint32_t line, alFunction function, ReturnType& ret, ALCdevice* device, Params... params)
+auto alCallImpl(const char* filename, uint32_t line, alFunction function, ReturnType& ret, Params... params)
+    -> typename std::enable_if_t<!std::is_same_v<void, decltype(function(params...))>, bool>
+{
+    ret = function(std::forward<Params>(params)...);
+    return check_al_errors(filename, line);
+}
+
+template<typename alFunction, typename... Params>
+auto alCallImpl(const char* filename, uint32_t line, alFunction function, Params... params)
+    -> typename std::enable_if_t<std::is_same_v<void, decltype(function(params...))>, bool>
+{
+    function(std::forward<Params>(params)...);
+    return check_al_errors(filename, line);
+}
+
+bool check_alc_errors(const char* filename, uint32_t line, ALCdevice* device);
+
+template<typename alcFunction, typename ReturnType, typename... Params>
+auto alcCallImpl(const char* filename, uint32_t line, alcFunction function, ReturnType& ret, ALCdevice* device, Params... params)
     -> typename std::enable_if_t<!std::is_same_v<void, decltype(function(params...))>, bool>
 {
     ret = function(std::forward<Params>(params)...);
     return check_alc_errors(filename, line, device);
 }
 
-template<typename alFunction, typename... Params>
-auto alcCallImpl(const char* filename, uint32_t line, alFunction function, ALCdevice* device, Params... params)
+template<typename alcFunction, typename... Params>
+auto alcCallImpl(const char* filename, uint32_t line, alcFunction function, ALCdevice* device, Params... params)
     -> typename std::enable_if_t<std::is_same_v<void, decltype(function(params...))>, bool>
 {
     function(std::forward<Params>(params)...);
     return check_alc_errors(filename, line, device);
 }
 
+#define alCall(function, ...) alCallImpl(__FILE__, __LINE__, function, __VA_ARGS__)
 #define alcCall(function, device, ...) alcCallImpl(__FILE__, __LINE__, function, device, __VA_ARGS__)
